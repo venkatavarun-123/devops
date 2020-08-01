b40a4462ca22e424e323db2bafa581f4a18bdf21
venkatavarun <bvvarun123@gmail.com>
2020-08-01 07:33:44 +0000

    file3 modified

--- file3.java
+++ file3.java
@@ -1,4 +1,11 @@
-class demo{
-	public static viod main(string args[])
-		System.out.println("file3 created")
-}
+file3 created and commit message corrected
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# Date:      Sat Aug 1 07:22:13 2020 +0000
+#
+# On branch master
+# Changes to be committed:
+#	new file:   file3.java
+#
